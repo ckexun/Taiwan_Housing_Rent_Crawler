03ed6ac9320acdbb0a5f1f90d4f6142a13aaa833
ckexun <clairetgn9315@gmail.com>
2025-08-10 14:46:58 +0800

    Update deploy-shiny.yml

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -15,8 +15,8 @@ repo   <- "Taiwan_Housing_Rent_Crawler"
 branch <- "main"
 
 api_csv_url <- sprintf("https://raw.githubusercontent.com/%s/%s/%s/api.csv", owner, repo, branch)
-gov_csv_url <- sprintf("https://raw.githubusercontent.com/%s/%s/%s/%s/data/桃園市各地所及工作站服務據點.csv", owner, repo, branch, "shiny_app")
-committee_csv_url <- sprintf("https://raw.githubusercontent.com/%s/%s/%s/%s/data/桃園市公寓大廈管理委員會清冊(中壢區_)v2.csv", owner, repo, branch, "shiny_app")
+gov_csv_url <- sprintf("https://raw.githubusercontent.com/ckexun/Taiwan_Housing_Rent_Crawler/refs/heads/main/shiny_app/data/%E6%A1%83%E5%9C%92%E5%B8%82%E5%90%84%E5%9C%B0%E6%89%80%E5%8F%8A%E5%B7%A5%E4%BD%9C%E7%AB%99%E6%9C%8D%E5%8B%99%E6%93%9A%E9%BB%9E.csv")
+committee_csv_url <- sprintf("https://raw.githubusercontent.com/ckexun/Taiwan_Housing_Rent_Crawler/refs/heads/main/shiny_app/data/%E6%A1%83%E5%9C%92%E5%B8%82%E5%85%AC%E5%AF%93%E5%A4%A7%E5%BB%88%E7%AE%A1%E7%90%86%E5%A7%94%E5%93%A1%E6%9C%83%E6%B8%85%E5%86%8A(%E4%B8%AD%E5%A3%A2%E5%8D%80_)v2.csv")
 
 read_utf8_csv <- function(path_or_url) {
   readr::read_csv(path_or_url, show_col_types = FALSE, locale = locale(encoding = "UTF-8"))
